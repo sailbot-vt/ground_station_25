5c24f327becbbb663f293f4912a24039fa0959eb
Barrett Wise <barrettw@fastmail.com>
2025-05-19 00:00:57 -0400

    fix issue fr this time

--- run.sh
+++ run.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-# Check if the bin directory exists
-if [ ! -d "bin" ]; then
-    # Create the bin directory
-    mkdir bin
-    if [ $? -ne 0 ]; then
-        echo "Failed to create bin directory. Please check your permissions."
-        exit 1
-    fi
-    echo "Created bin directory."
-fi
+mkdir -p bin
 
-if [ -f "last_build_time.txt" ]; then
-    last_build_time=$(cat last_build_time.txt)
+if [ -f "bin/server" ]; then
+    last_build_time=$(cat "last_build_time.txt")
     # if last_build_time is older than 1 hour
     if [ $(($(date +%s) - last_build_time)) -gt 3600 ]; then
         echo "Server binary out of date. Rebuilding..."
@@ -33,15 +24,20 @@ if [ -f "last_build_time.txt" ]; then
         echo "last_build_time.txt is less than 1 hour old. Skipping build."
     fi
 else
-    echo "last_build_time.txt not found. Building the Go server..."
+    echo "Server binary not found. Building..."
     # Check if Go is on the PATH
     if ! command -v go &> /dev/null; then
         echo "Go is not installed. Please install Go to run this script."
         exit 1
     fi
     go build -o bin/server src/web_engine/server.go
-    echo "Go server built successfully."
-    date +%s > last_build_time.txt
+    if [ $? -ne 0 ]; then
+        echo "Failed to build the Go server. Please check your Go installation."
+        exit 1
+    else
+        echo "Go server built successfully."
+        date +%s > last_build_time.txt
+    fi
 fi
 
 # Start the Go server in the background
